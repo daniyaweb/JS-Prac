a753c84172582c8bd8e3da19d45a9d83e14855ed
daniyaweb <syedaaeman09@gmail.com>
2026-06-09 16:29:25 +0500

    solutions

--- script.js
+++ script.js
@@ -2,7 +2,42 @@
 // Ask: Age
 // Rules:
 // Under 12 → Not Allowed 
-// 2–17 → Rs. 200/hour 
+// 12–17 → Rs. 200/hour 
 // 18+ → Rs. 400/hour
 
-var = prompt ()
+// var age = prompt("Enter your Age");
+
+// if (age < 12) {
+//     console.log("Not Allowed");
+    
+// }
+//     else if (age >= 12 && age < 18) {
+//     console.log("Rs. 200/ hour");
+    
+// }
+// else {
+//     console.log("Rs. 400/ hour");
+    
+// }
+
+
+
+// Shopping Discount
+// Rules:
+// Bill > 5000 → 20% discount 
+// Bill > 3000 → 10% discount 
+// Otherwise → No discount 
+
+// var bill = prompt("How much is your Bill")
+//     if( bill > 5000){
+//         console.log("20% Discount");
+        
+//     }
+//         else if( bill >= 3000 && bill <=5000 ){
+//             console.log("10% Discount");
+            
+//         }
+//         else{
+//            console.log("No Discount");
+//         }
+
