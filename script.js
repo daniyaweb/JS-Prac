122d1d747089bb9c4cfafe6fdb064ee3349bcea2
daniyaweb <syedaaeman09@gmail.com>
2026-06-13 16:34:28 +0500

    loopPrac

--- script.js
+++ script.js
@@ -41,3 +41,43 @@
 //            console.log("No Discount");
 //         }
 
+
+// Online Game Level Checker
+// Ask: Player's score
+// Rules:
+// Score 0–100 → Level 1 (Beginner)
+// Score 101–300 → Level 2 (Intermediate)
+// Score 301–500 → Level 3 (Advanced)
+// Score 501+ → Level 4 (Master)
+// Output: Display the player's level
+
+// var score = prompt("What is your current score?")
+//     if(score >=0 && score <= 100){
+//         console.log("Beginner");
+// }
+//         else if(score >=101 && score <= 300){
+//         console.log("Intermediate");
+//     }
+// else if(score >= 301 && score <= 500){
+//         console.log("Advanced");
+// }
+// else if(score <0 ){
+//     console.log("Invalid Score");
+    
+// }
+// else{console.log("Master");
+// }
+
+
+// Multiplication Table Generator
+// Ask: Number (for which table is required)
+// Rules:
+// •	Use a loop (for or while) only
+// •	Print multiplication table from 1 to 10
+// Output: Display the full multiplication table of the given number up to 10
+
+var num= prompt("Enter a Number")
+for (let i= 1; i<= 10; i++){
+    console.log(num, "X" , i , "=" , num* i);
+    
+}
