bc31b3ba6ff0dada06abc9c2dba68d6105156f6d
daniyaweb <syedaaeman09@gmail.com>
2026-06-26 22:24:13 +0500

    Added Product Availability Checker Prac

--- script.js
+++ script.js
@@ -84,61 +84,61 @@ for (let i = 1; i <= 10; i++) {
 }
 
 
-    // Ques:05 Write a function named calculateSalary that calculates an employee’s final salary. The function should take three inputs: base salary, bonus amount, and tax percentage.
+// Ques:05 Write a function named calculateSalary that calculates an employee’s final salary. The function should take three inputs: base salary, bonus amount, and tax percentage.
 
-    // First, combine the base salary and bonus to get the total income. Then apply tax on this total income using the given percentage. Finally, return the remaining amount after tax deduction as the final salary.
+// First, combine the base salary and bonus to get the total income. Then apply tax on this total income using the given percentage. Finally, return the remaining amount after tax deduction as the final salary.
 
-    function calculateSalary(baseSalary, bonus, taxPercent) {
-        return (baseSalary + bonus) - ((baseSalary + bonus) * taxPercent / 100)
-    }
+function calculateSalary(baseSalary, bonus, taxPercent) {
+    return (baseSalary + bonus) - ((baseSalary + bonus) * taxPercent / 100)
+}
 
-    console.log(calculateSalary(30000, 10000, 10));
+console.log(calculateSalary(30000, 10000, 10));
 
-    // Ques:06 Write a function named convertToPKR that converts an amount from US Dollars (USD) to Pakistani Rupees (PKR). The function should take two inputs: the amount in dollars and the exchange rate.
+// Ques:06 Write a function named convertToPKR that converts an amount from US Dollars (USD) to Pakistani Rupees (PKR). The function should take two inputs: the amount in dollars and the exchange rate.
 
-    // Multiply the dollar amount by the exchange rate and return the result as the amount in PKR.
-
-
-    function convertToPkr(dollar, rate) {
-
-        return (dollar * rate)
-    }
-
-    console.log(convertToPkr(300, 280));
+// Multiply the dollar amount by the exchange rate and return the result as the amount in PKR.
 
 
-    // Ques:07 Write a function named fuelCost that calculates the total fuel expense for a trip. The function should take two inputs: the number of kilometers traveled and the fuel cost per kilometer.
+function convertToPkr(dollar, rate) {
 
-    // Multiply the distance traveled by the cost per kilometer and return the total fuel expense.
+    return (dollar * rate)
+}
 
-    function fuelCost(kilometers, costPerKilometer) {
-
-        return (kilometers * costPerKilometer)
-    };
-
-    console.log(fuelCost(50, 12));
+console.log(convertToPkr(300, 280));
 
 
-    // Ques:08 Write a function named rectanglePerimeter that calculates the perimeter of a rectangle. The function should take two inputs: length and width.
+// Ques:07 Write a function named fuelCost that calculates the total fuel expense for a trip. The function should take two inputs: the number of kilometers traveled and the fuel cost per kilometer.
 
-    // Use the standard perimeter formula of a rectangle to compute the result and return the final value.
+// Multiply the distance traveled by the cost per kilometer and return the total fuel expense.
 
-    function rectanglePerimeter(length, width) {
+function fuelCost(kilometers, costPerKilometer) {
 
-        return (2 * (length + width))
-    }
+    return (kilometers * costPerKilometer)
+};
 
-    console.log(rectanglePerimeter(10, 5))
+console.log(fuelCost(50, 12));
 
 
-    
+// Ques:08 Write a function named rectanglePerimeter that calculates the perimeter of a rectangle. The function should take two inputs: length and width.
+
+// Use the standard perimeter formula of a rectangle to compute the result and return the final value.
+
+function rectanglePerimeter(length, width) {
+
+    return (2 * (length + width))
+}
+
+console.log(rectanglePerimeter(10, 5))
+
+
+
 
 // Ques:09 Employee Final Salary Calculator
 // An employee's final salary needs to be calculated based on their basic salary, overtime pay, late deductions, and absent deductions. Write a program that creates a function to calculate the final salary by adding the overtime pay to the basic salary and subtracting the late and absent deductions. Return the final payable salary.
 
 var basicSalary = 50000;
 
-var overTimeHours = 3; 
+var overTimeHours = 3;
 var overTimeRate = 5000;
 
 var absentDays = 2;
@@ -147,20 +147,20 @@ var dailySalaryRates = 1600;
 var lateHours = 2;
 var lateDeductionRate = 1000;
 
-function overTimePay(overTimeHours , overTimeRate){
-    return( overTimeHours * overTimeRate)
+function overTimePay(overTimeHours, overTimeRate) {
+    return (overTimeHours * overTimeRate)
 }
 
-function lateDeduction(lateHours , lateDeductionRate){
-    return(lateHours * lateDeductionRate)
+function lateDeduction(lateHours, lateDeductionRate) {
+    return (lateHours * lateDeductionRate)
 }
 
-function absentDeduction(absentDays , dailySalaryRates){
-    return(absentDays * dailySalaryRates)
+function absentDeduction(absentDays, dailySalaryRates) {
+    return (absentDays * dailySalaryRates)
 }
 
-function calculateFinalSalary(basicSalary, overTimePay, lateDeduction, absentDeduction){
-    return((basicSalary + overTimePay) - (lateDeduction + absentDeduction))
+function calculateFinalSalary(basicSalary, overTimePay, lateDeduction, absentDeduction) {
+    return ((basicSalary + overTimePay) - (lateDeduction + absentDeduction))
 }
 
 console.log(calculateFinalSalary(
@@ -173,9 +173,9 @@ console.log(calculateFinalSalary(
 // Ques:10 Display Student Information
 // Create an array named stuInfo that contains a student's name, age, and city. Use a for loop to go through each element of the array and display every value in the console, one at a time. The program should print all the information stored in the array without accessing the elements manually by their index.
 
-var stuInfo = ["sheraz" , 25 , "karachi"]
+var stuInfo = ["sheraz", 25, "karachi"]
 
-for (var i = 0; i< stuInfo.length; i++){
+for (var i = 0; i < stuInfo.length; i++) {
     console.log(stuInfo[i]);
 
 }
@@ -187,8 +187,34 @@ for (var i = 0; i< stuInfo.length; i++){
 // Use an array to store item prices and a loop to calculate the sum of all values.
 
 var cartTotal = [250, 500, 1000, 150, 300]
-var sum= 0
+var sum = 0
 
-for( var i= 0; i < cartTotal.length; i++)
-     sum += cartTotal[i]
-    console.log(sum);
+for (var i = 0; i < cartTotal.length; i++)
+    sum += cartTotal[i]
+console.log(sum);
+
+// Ques:12 Product Availability Checker
+// An online store maintains a list of available products in an array. The store needs a simple system to check whether a specific product is currently available or not.
+// Write a program that creates a function to search for a product name in the product list. The function should check each product one by one using a loop and return true if the product is found. If the product does not exist in the list after checking all items, the function should return false. 
+
+
+var products = ["Phone", "Laptop", "Camera", "HeadPhones", "Keyboard", "Mouse"];
+function searchProduct(products, searchItem) {
+
+    for (var i = 0; i < products.length; i++) {
+        if (products[i] === searchItem) {
+
+            return true;
+        }
+
+    }
+
+    return false;
+
+}
+
+console.log(searchProduct(products, "Laptop"));
+// true
+
+console.log(searchProduct(products, "Tablet"));
+// false
